3f415304bb34bec875ddc98bfc60bd2e3a96221d
reyhaneh.sp <reyhaneh.sanat@yahoo.co.uk>
2021-04-13 17:07:01 +0430

    Updates

---
+++ precache-manifest.fdb5e35240f114eef405a87ba205844a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4dbdefecac29b6f450c273bbebc16a",
+    "url": "/myFirstReactjs/index.html"
+  },
+  {
+    "revision": "0b69913dd1451d945a21",
+    "url": "/myFirstReactjs/static/css/2.234d03a3.chunk.css"
+  },
+  {
+    "revision": "1c942c548178ec6de7e7",
+    "url": "/myFirstReactjs/static/css/main.023ac346.chunk.css"
+  },
+  {
+    "revision": "0b69913dd1451d945a21",
+    "url": "/myFirstReactjs/static/js/2.fc866eb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myFirstReactjs/static/js/2.fc866eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c942c548178ec6de7e7",
+    "url": "/myFirstReactjs/static/js/main.caa95571.chunk.js"
+  },
+  {
+    "revision": "1d9ae6990213485b4bc3",
+    "url": "/myFirstReactjs/static/js/runtime-main.3893c584.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myFirstReactjs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myFirstReactjs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myFirstReactjs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myFirstReactjs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myFirstReactjs/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myFirstReactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
